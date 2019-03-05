15d894b7996b71a8434db8ebb9b6a08bf0a5679e
Travis Martin <travisemerymartin@gmail.com>
2019-03-05 19:51:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,9 @@ end
 
 def monopoly_with_second_tier
   monopoly={railroads:{}}
-  # monopoly[:railroads][:pieces]=4
-  # monopoly[:railroads][:names]={}
+  monopoly[:railroads][:pieces]=4
+  monopoly[:railroads][:names]={}
+  monopoly[:railroads][:rent_in_dollars]={}
 end
 
 def monopoly_with_third_tier
